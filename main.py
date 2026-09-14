ad423c7131a0cad9953ea9bd2ab449aeac1491c5
Videirafoo <videirafo@gmail.com>
2026-09-14 14:11:40 -0300

    Organiza execucao sequencial dos exercicios

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ from exercicio4 import exercicio4
 from exercicio5 import exercicio5
 from exercicio6 import exercicio6
 
-exercicio1()
-exercicio2()
-exercicio3()
-exercicio4()
-exercicio5()
-exercicio6()
+exercicios = [
+	exercicio1,
+	exercicio2,
+	exercicio3,
+	exercicio4,
+	exercicio5,
+	exercicio6,
+]
+
+print("Lista de Exercícios de Algoritmos")
+print("Os exercícios serão executados em sequência.")
+
+for numero, exercicio in enumerate(exercicios, start=1):
+	exercicio()
+
+	if numero < len(exercicios):
+		input(
+			f"Pressione Enter para continuar para o próximo exercício "
+			f"({numero + 1})..."
+		)
+		print("\n" + "-" * 50 + "\n")
